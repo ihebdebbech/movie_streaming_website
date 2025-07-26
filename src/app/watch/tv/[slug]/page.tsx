e8c95a14f049f2bce357d2eb6ba504a9d8b67354
ihebdebbech <ihebdebbech20@gmail.com>
2025-07-26 01:53:37 +0100

    initialproject

--- src/app/watch/tv/[slug]/page.tsx
+++ src/app/watch/tv/[slug]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import EmbedPlayer from '@/components/watch/embed-player';
 
 export const revalidate = 3600;
-
+//https://vidsrc.su/embed/tv/76331/1/9
 export default function Page({ params }: { params: { slug: string } }) {
   const id = params.slug.split('-').pop();
   return <EmbedPlayer url={`https://vidsrc.cc/v2/embed/tv/${id}`} />;
